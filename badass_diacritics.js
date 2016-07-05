23f82cd8ced40c3279ea1ae0896b723c40b09fe4
Christoph Matthies <christoph.matthies@gmail.com>
2016-07-06 00:01:43 +0200

    Add badass comment on diacritics source.

--- badass_diacritics.js
+++ badass_diacritics.js
@@ -1,3 +1,5 @@
+// Adapted from https://gist.github.com/yeah/1283961
+
 var badass_diacritics = [
       {'base':'A', 'letters':["\u0041","\u24B6","\u00C0","\u00C1","\u00C2","\u1EA6","\u1EA4","\u1EAA","\u1EA8","\u00C3","\u0100","\u0102","\u1EB0","\u1EAE","\u1EB4","\u1EB2","\u0226","\u01E0","\u01DE","\u1EA2","\u00C5","\u01FA","\u01CD","\u0200","\u0202","\u1EA0","\u1EAC","\u1EB6","\u1E00","\u0104","\u023A","\u2C6F"]},
       {'base':'AA','letters':["\uA732"]},
